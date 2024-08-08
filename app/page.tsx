421a993b65dec7a66212cd63bb38c4b3fcb601a4
patacsilpam <pamelampatacsil@gmail.com>
2024-08-08 21:08:35 +0800

    page & layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import { navItems } from "@/data/globals";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Image from "next/image";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 export default function Home() {
   return (
-    <main className="bg-[#000319] min-h-screen p-24">
-      <FloatingNav navItems={navItems} />
-      <Hero />
+    <main className="bg-gradient-to-t bg-[#000319] to-indigo-500 relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Projects />
+      </div>
     </main>
   );
 }
