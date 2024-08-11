a92bc72cdafe4d46a8e61ad7e01538f56f00e5c5
patacsilpam <pamelampatacsil@gmail.com>
2024-08-11 21:12:30 +0800

    data

--- data/globals.ts
+++ data/globals.ts
@@ -18,31 +18,73 @@ export const navItems = [
 
 export const projects = [
   {
+    title: "Stripe",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+    skills: [
+      { name: "HTML", image: "" },
+      { name: "TailwindCSS", image: "" },
+    ],
+  },
+  {
+    title: "Netflix",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+    skills: [
+      { name: "laravel", image: "" },
+      { name: "php", image: "" },
+    ],
+  },
+  {
+    title: "Google",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+    skills: [
+      { name: "javascript", image: "" },
+      { name: "typescript", image: "" },
+    ],
+  },
+  {
+    title: "Meta",
+    description:
+      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+    link: "https://meta.com",
+    skills: [
+      { name: "java", image: "" },
+      { name: "flutter", image: "JavaScript" },
+    ],
+  },
+];
+export const tools = [
+  {
     id: 1,
-    content: "About",
-    classname: "#about",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTihRZ3zBxvaePbfLPnKYkidKYRnMx0wTk9TA&s",
+    skills: [
+      { name: "HTML", image: "" },
+      { name: "TailwindCSS", image: "" },
+    ],
   },
   {
     id: 2,
-    content: "Projects",
-    classname: "#projects",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTihRZ3zBxvaePbfLPnKYkidKYRnMx0wTk9TA&s",
+    skills: [
+      { name: "laravel", image: "" },
+      { name: "php", image: "" },
+    ],
   },
   {
     id: 3,
-    content: "Experience",
-    classname: "#experience",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTihRZ3zBxvaePbfLPnKYkidKYRnMx0wTk9TA&s",
+    skills: [
+      { name: "javascript", image: "" },
+      { name: "typescript", image: "" },
+    ],
   },
   {
     id: 4,
-    content: "Experience",
-    classname: "#experience",
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTihRZ3zBxvaePbfLPnKYkidKYRnMx0wTk9TA&s",
+    skills: [
+      { name: "java", image: "" },
+      { name: "flutter", image: "JavaScript" },
+    ],
   },
 ];
