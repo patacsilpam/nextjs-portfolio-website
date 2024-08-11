10d86487a6154d593574545e1193da714ee93cf7
patacsilpam <pamelampatacsil@gmail.com>
2024-08-11 21:13:06 +0800

    ui:bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -28,7 +28,7 @@ export const BentoGridItem = ({
   icon,
   image,
 }: {
-  id: number;
+  id?: number;
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
@@ -42,8 +42,15 @@ export const BentoGridItem = ({
         " row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        //   add these two
+        //   you can generate the color from here https://cssgradient.io/
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
     >
-      {id === 1 && <div className="  ">dsfd</div>}
+      {id === 1 && <div className="  bg-white">dsfd</div>}
       {id === 3 && <div>copy email address</div>}
     </div>
   );
