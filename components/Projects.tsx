1ffe7da2a94d7b8d8a6b333da5f20eff6e132c34
patacsilpam <pamelampatacsil@gmail.com>
2024-08-11 21:12:54 +0800

    page: projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,49 +1,15 @@
-import { cn } from "@/lib/utils";
-import React from "react";
-import { BentoGrid, BentoGridItem } from "@/components/ui/BentoGrid";
-
-export function BentoGridSecondDemo() {
+"use client";
+import { HoverEffect } from "@/components/ui/CardHoverEffect";
+import { projects } from "@/data/globals";
+export default function CardHoverEffectDemo() {
   return (
-    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          className={item.className}
-        />
-      ))}
-    </BentoGrid>
+    <div className="max-w-5xl mx-auto px-8">
+      <div className="mt-44 text-center">
+        <h1 className="text-white text-5xl font-bold">
+          A small of recent projects
+        </h1>
+      </div>
+      <HoverEffect items={projects} />
+    </div>
   );
 }
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-);
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    className: "md:col-span-2",
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    className: "md:col-span-2",
-  },
-];
