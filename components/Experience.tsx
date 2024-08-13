12d2997b4c28b53865a65162034611949d7f81a4
patacsilpam <pamelampatacsil@gmail.com>
2024-08-13 22:33:00 +0800

    feat: experience page

---
+++ components/Experience.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="mt-24">
+      <h1 className="text-white text-5xl font-bold text-center">
+        My <span className="text-purple-400">work experience</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Experience;
