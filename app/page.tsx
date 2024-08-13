700c53bcbbbf2aace695856f07cfa03cda8a5d1e
patacsilpam <pamelampatacsil@gmail.com>
2024-08-13 22:32:51 +0800

    feat: upate bg color

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ import Image from "next/image";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import BentoGridSecondDemo from "@/components/ui/BentoGridSecondDemo";
 import Projects from "@/components/Projects";
+import { HeroHighlight } from "@/components/ui/HeroHighlight";
+import Experience from "@/components/Experience";
 export default function Home() {
   return (
-    <main className="bg-gradient-to-t bg-[#000319] to-indigo-500 relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="  bg-gradient-to-b from-[#000319] via-[#03061b] to-[#070a25] relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <BentoGridSecondDemo />
         <Projects />
+        <Experience />
       </div>
     </main>
   );
