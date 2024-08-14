b669594b7c83d6e3947afb3fc30d358c39db57fe
patacsilpam <pamelampatacsil@gmail.com>
2024-08-14 22:10:21 +0800

    feat: experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import ExperienceCard from "@/components/ui/ExperienceCard";
 const Experience = () => {
   return (
     <div className="mt-24">
       <h1 className="text-white text-5xl font-bold text-center">
         My <span className="text-purple-400">work experience</span>
       </h1>
+      <br />
+      <br />
+      <ExperienceCard />
     </div>
   );
 };
