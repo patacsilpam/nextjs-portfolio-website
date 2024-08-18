3e34066fa7243d54014c17acdc44d140d400f05d
patacsilpam <pamelampatacsil@gmail.com>
2024-08-18 19:49:53 +0800

    feat: card

--- components/ui/ExperienceCard.tsx
+++ components/ui/ExperienceCard.tsx
@@ -7,7 +7,7 @@ const ExperienceCard = () => {
         <div className="flex flex-col justify-center items-center">
           <div
             key={key}
-            className="max-w-lg w-full  relative text-neutral-200 row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col"
+            className="max-w-lg w-full h-80 relative text-neutral-200 row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col"
             style={{
               //   add these two
               //   you can generate the color from here https://cssgradient.io/
