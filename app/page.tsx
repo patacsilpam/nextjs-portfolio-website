0ee4b1e872e48658c179c85fc99ff17ef51f7750
patacsilpam <pamelampatacsil@gmail.com>
2024-08-31 23:44:10 +0800

    page

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="bg-[#0d0e0e]   relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+      <div className="">
         <FloatingNav navItems={navItems} />{" "}
         {/*bg-gradient-to-b from-[#030712] via-[#03061b] to-[#070a25]*/}
         <Hero />
