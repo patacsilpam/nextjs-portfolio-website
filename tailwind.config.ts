af4f045d18e745db8645508726bc25954907ad51
patacsilpam <pamelampatacsil@gmail.com>
2024-09-01 22:21:19 +0800

    style: textUnderlineOffset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      textUnderlineOffset: {
+        10: "20px",
+      },
       fontFamily: {
         lexend: ["Lexend", "sans-serif"],
       },
