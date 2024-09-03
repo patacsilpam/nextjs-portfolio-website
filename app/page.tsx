2ab0f1953821b79f53d06395cf5c7ccacc56b9cc
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-03 23:55:16 +0800

    app

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,15 @@ import Projects from "@/components/Projects";
 import { HeroHighlight } from "@/components/ui/HeroHighlight";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
+import Services from "@/components/Services"
 export default function Home() {
   return (
-    <main className="bg-[#0d0e0e]   relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="bg-[#f7f7f7]   relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="">
         <FloatingNav navItems={navItems} />{" "}
         {/*bg-gradient-to-b from-[#030712] via-[#03061b] to-[#070a25]*/}
         <Hero />
-        <BentoGridSecondDemo />
+        <Services />
         <Projects />
         <Experience />
         <Footer />
