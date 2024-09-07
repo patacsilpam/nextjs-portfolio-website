32736108638b98602f85349c1f05b090121aca0a
patacsilpam <pamelampatacsil@gmail.com>
2024-09-07 22:43:51 +0800

    change bg color

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
-    <div className='flex flex-1 w-screen h-full min-h-[600px] rounded-xl bg-[#eaeaee]'>Services</div>
-  )
-}
+    <div className="p-5">
+      <div className="bg-[#000000] flex flex-1 w-screen h-full min-h-[600px] rounded-xl">
+        Serices
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
