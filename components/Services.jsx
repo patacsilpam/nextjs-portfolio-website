ff6dbed0b69f397c94a2071114ecee18db8bbe35
patacsilpam <pamelampatacsil@gmail.com>
2024-09-08 19:21:27 +0800

    feat: Enhance Services component with dynamic content and improved layout

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-
+import { services } from "@/data/globals";
 const Services = () => {
   return (
-    <div className="p-5">
-      <div className="bg-[#000000] flex flex-1 w-screen h-full min-h-[600px] rounded-xl">
-        Serices
+    <div className="bg-[#000000] flex flex-col justify-center items-center  w-screen min-h-[768px] md:p-20 p-10 gap-10">
+      <div className="text-white text-center space-y-5">
+        <h1 className="md:text-5xl text-3xl  font-light">
+          Envision. Craft. Engage
+        </h1>
+        <div className="max-w-2xl">
+          <p className="font-openSans font-light md:text-xl text-sm text-[#f7f7f7] text">
+            I turn your vision into reality with precise design and development,
+            creating engaging digital experiences that resonate with your
+            audience.
+          </p>
+        </div>
+      </div>
+      <div className=" flex md:flex-row justify-center items-center flex-col gap-10 ">
+        {services.map((service, key) => (
+          <div
+            key={key}
+            className="text-white md:w-[400px] w-full min-h-[300px] h-full p-5 bg-[#141414]"
+          >
+            <span>{<service.icon />}</span>
+            <p className="text-2xl mt-8 mb-3 ">{service.title}</p>
+            <hr />
+            <p className="text-sm font-light mt-12 text-[#e3e2e2]">
+              {service.description}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
