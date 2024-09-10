726879fe5a9b76101777f9357b38104fa81e79c8
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-11 06:55:51 +0800

    feat: changed content

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { HoverEffect } from "@/components/ui/CardHoverEffect";
-import { projects } from "@/data/globals";
-export default function CardHoverEffectDemo() {
+import LandingPage from "./ui/LandingPage";
+export default function Projects() {
   return (
-    <div className="max-w-5xl mx-auto px-8">
-      <div className="mt-44 text-center">
-        <h1 className="text-white text-5xl font-bold">
-          A small of <span className="text-purple-400">recent projects</span>
-        </h1>
+    <div className="min-h-[768px] h-full bg-[#000000] flex flex-col items-center justify-center p-10">
+      <h1 className="text-white font-openSans text-5xl max-w-2xl w-full text-center">Take a look at the latest project that I've crafted.</h1>
+      <div className="grid place-items-center lg:grid-cols-2 grid-cols-1 gap-10 p-20">
+        <LandingPage/>
+        <LandingPage/>
       </div>
-      <HoverEffect items={projects} />
     </div>
   );
 }
