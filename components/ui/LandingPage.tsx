a8ed3d4d127d8ea13112c603a1985e73359a8185
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-11 06:56:27 +0800

    feat; landing page project ui

---
+++ components/ui/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className="bg-[#EBEDF6] md:h-[450px] h-[350px] w-[450px] relative overflow-hidden rounded-sm">
+        <div>
+          <img src="/images/projects/landing-page-4.png" alt="" className="h-[250px] absolute z-20 top-10 left-10  transform -rotate-12"
+        style={{ transform: 'rotate(-40deg)' }} />
+          
+        </div>
+        <div className="-mt-20">
+          <img src="/images/projects/landing-page-2.png" alt="" className="-rotate-12 scale-150"/>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
