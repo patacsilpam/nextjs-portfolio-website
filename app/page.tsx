9b744812cfc2ea4d70fad4a88ff73e795c8daa91
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-11 06:55:29 +0800

    feat: changed content

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import { navItems } from "@/data/globals";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import BentoGridSecondDemo from "@/components/ui/BentoGridSecondDemo";
-import Projects from "@/components/Projects";
-import { HeroHighlight } from "@/components/ui/HeroHighlight";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
 import Services from "@/components/Services";
+import TechStack from "@/components/TechStack";
+import Projects from "@/components/Projects";
 export default function Home() {
   return (
     <main className="bg-[#f7f7f7] relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 ">
@@ -15,9 +11,8 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Services />
-        <Projects />
-        <Experience />
-        <Footer />
+        <TechStack/>
+        <Projects/>
       </div>
     </main>
   );
