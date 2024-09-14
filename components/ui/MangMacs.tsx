543ddeebbf4288f7cdb6d8198ec8b948efc7b2fb
patacsilpam <pamelampatacsil@gmail.com>
2024-09-14 21:59:10 +0800

    feat: image rotation

--- components/ui/MangMacs.tsx
+++ components/ui/MangMacs.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
     <div className="bg-[#EBEDF6] md:h-[400px] h-[350px] w-[400px] relative overflow-hidden rounded-sm">
-        <div>
-          <img src="/images/projects/mangmacs-admin-2.png" alt="" className="mt-24  h-[250px] absolute z-20 top-10 left-20 shadow-md"
-        style={{ transform: 'rotate(-12deg)' }} />
-          
-        </div>
-        <div className="">
-          <img src="/images/projects/mangmacs-admin-1.png" alt="" className="-rotate-12 -ml-2 h-[300px] w-[850px] scale-110 shadow-md" />
-        </div>
+      <div>
+        <img
+          src="/images/projects/mangmacs-admin-2.png"
+          alt=""
+          className="  absolute z-20 shadow-md  mt-56 ml-20"
+          style={{ transform: "rotate(-12deg)", scale: "1.5" }}
+        />
+      </div>
+      <div className="">
+        <img
+          src="/images/projects/mangmacs-admin-1.png"
+          alt=""
+          className="shadow-md mt-10"
+          style={{ transform: "rotate(-12deg)", scale: "1.5" }} //dasboard
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
