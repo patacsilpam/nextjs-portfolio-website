49479817232b5fb1e3575642fc93942ce9c3d760
patacsilpam <pamelampatacsil@gmail.com>
2024-09-14 21:57:49 +0800

    feat: components

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Services from "@/components/Services";
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="bg-[#f7f7f7] relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 ">
@@ -11,8 +13,10 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Services />
-        <TechStack/>
-        <Projects/>
+        <TechStack />
+        <Projects />
+        <Contact />
+        <Footer />
       </div>
     </main>
   );
