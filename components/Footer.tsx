5be36357ab8abc04131edd86b09412e6dccdec77
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-18 21:52:51 +0800

    feat: added cursor pointer hover

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,17 +7,17 @@ const Footer = () => {
   const year = new Date();
   const currentYear = year.getFullYear();
   return (
-    <div className="p-10 flex flex-row justify-between">
+    <div className="p-10 flex md:flex-row flex-col-reverse md:justify-between justify-center items-center md:text-left text-center">
       <span className="flex flex-col">
         <p>Built by Pam Patacsil</p>
         <p>&copy; {currentYear}</p>
       </span>
-      <ul>
+      <ul className="flex flex-row gap-5">
         {socialList.map((item, key) => (
           <a
             key={key}
-            href={item.link}
-            className="font-light mx-3"
+            href="https://github.com/patacsilpam/nextjs-portfolio-website/blob/main/components/Footer.tsx"
+            className="cursor-pointer "
             target="_blank"
           >
             {item.title}
